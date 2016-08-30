3c7d4cdd03ff26bd7f4a9bd59a210bf66120c6e8
DrSmugleaf <javier.frayluis@gmail.com>
2016-08-30 20:48:48 +0200

    Cleanup

--- banterbot.js
+++ banterbot.js
@@ -1,20 +1,16 @@
 "use strict"
 const Discord = require("discord.js");
 const discord = new Discord.Client();
+const fs = require("fs");
 const MessageHandler = require("./libs/handler/messagehandler");
 const messagehandler = new MessageHandler();
-const TranslatorHandler = require("./libs/handler/translatorhandler");
-const translatorhandler = new TranslatorHandler();
 const token = process.env.DISCORD_TOKEN;
 
 console.log("Running banterbot.js");
 
 discord.on("message", function(msg) {
   if(msg.author.bot) { return; };
-  translatorhandler.translate(discord, msg, {"development": "Spanish", "serbia": "English"});
-  //messagehandler.handleMessage(msg, discord);
+  messagehandler.handleMessage(msg, discord);
 });
-
-//translatorhandler.bridgeChannels(discord, {"development": "Spanish", "serbia": "English"});
 
 discord.loginWithToken(token);
--- libs/handler/messagehandler.js
+++ libs/handler/messagehandler.js
@@ -1,8 +1,8 @@
 "use strict"
 const CommandHandler = require("./commandhandler");
 const commandhandler = new CommandHandler();
-// const TranslatorHandler = require("./translatorhandler");
-// const translatorhandler = new TranslatorHandler();
+const TranslatorHandler = require("./translatorhandler");
+const translatorhandler = new TranslatorHandler();
 
 class MessageHandler {
   constructor() {
@@ -19,7 +19,7 @@ MessageHandler.prototype.handleMessage = function(msg, discord) {
       commandhandler.getCommand(msg, discord);
       break;
     default:
-      // translatorhandler.translate(msg, discord);
+      translatorhandler.translate(discord, msg, {"general": "Spanish", "serbia": "English"});
       break;
   };
 };
