d031b0058672683d5a6131fc68f619d4a1e95be9
DrSmugleaf <javier.frayluis@gmail.com>
2016-08-30 17:40:05 +0200

    Fixing web procfile

--- banterbot.js
+++ banterbot.js
@@ -1,11 +1,16 @@
 "use strict"
-const MessageHandler = require("./libs/handler/messagehandler");
-const messagehandler = new MessageHandler();
 const Discord = require("discord.js");
 const discord = new Discord.Client();
+const express = require("express");
+const app = express();
+const MessageHandler = require("./libs/handler/messagehandler");
+const messagehandler = new MessageHandler();
+const port = process.env.PORT || 3000;
 const token = process.env.DISCORD_TOKEN;
 
-console.log("Running banterbot.js");
+app.listen(port, function() {
+  console.log("Running banterbot.js");
+});
 
 discord.on("message", function(msg) {
   messagehandler.handleMessage(msg);
