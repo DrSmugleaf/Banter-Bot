cc0aefb95229cad6a68f610386f7f200baad0317
DrSmugleaf <javier.frayluis@gmail.com>
2016-09-01 15:24:07 +0200

    Removed mentions from translated message

--- libs/handler/translatorhandler.js
+++ libs/handler/translatorhandler.js
@@ -4,13 +4,19 @@ const arrayutil = new ArrayUtil();
 const translate = require("yandex-translate")(process.env.YANDEX_KEY);
 const key = process.env.YANDEX_KEY;
 
-class TranslatorHandler {};
+class TranslatorHandler {
+  constructor() {};
+};
 
 TranslatorHandler.prototype.translate = function(discord, msg, opts) {
   if(opts[msg.channel.name]) {
     let language = opts[msg.channel.name];
     for(let channel in opts) {
       if(opts[channel] == opts[msg.channel.name]) { continue; };
+      for(let mention in msg.mentions) {
+        let regex = new RegExp(`<@${msg.mentions[mention].id}>`,"g");
+        msg.content = msg.content.replace(regex, msg.mentions[mention].username);
+      };
       translate.translate(msg.content, {from: language, to: opts[channel]}, function(err, res) {
         if(err) { console.log(err); };
         discord.sendMessage(discord.channels.get("name", channel), "**" + msg.author.username + "**" + ": " + res.text[0]);
