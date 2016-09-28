fc743476b49c3ff686a56e9fcce929b3b4b6d593
DrSmugleaf <javier.frayluis@gmail.com>
2016-09-28 23:19:22 +0200

    Cleanup

--- libs/commands/index.js
+++ libs/commands/index.js
@@ -4,25 +4,6 @@
 
 "use strict"
 const constants = require("../util/constants")
-// const Color = require("./color")
-// const Coult = require("./coult")
-// const Erika = require("./erika")
-// const Help = require("./help")
-// const Josde = require("./josde")
-// const Magic8Ball = require("./magic8ball")
-// const Quote = require("./quote")
-// const Seifer = require("./seifer")
-// const Youtube = require("./youtube")
-//
-// exports.color = new Color()
-// exports.coult = new Coult()
-// exports.erika = new Erika()
-// exports.help = new Help()
-// exports.josde = new Josde()
-// exports.magic8ball = new Magic8Ball()
-// exports.quote = new Quote()
-// exports.seifer = new Seifer()
-// exports.youtube = new Youtube()
 
 let commands = {}
 for(let command in constants.commands) {
@@ -30,6 +11,4 @@ for(let command in constants.commands) {
     commands[command] = require("./" + command)
     exports[constants.commands[command][commandname]] = new commands[command]()
   }
-  // commands[command] = require("./" + command)
-  // exports[command] = new commands[command]()
 }
--- libs/handler/commandhandler.js
+++ libs/handler/commandhandler.js
@@ -5,18 +5,6 @@
 "use strict"
 const constants = require("../util/constants")
 const commands = require("../commands")
-// const Color = require("../commands/color")
-// const color = new Color()
-// const Coult = require("../commands/coult")
-// const coult = new Coult()
-// const Magic8Ball = require("../commands/magic8ball")
-// const magic8ball = new Magic8Ball()
-// const Youtube = require("../commands/youtube")
-// const youtube = new Youtube()
-// const Quote = require("../commands/quote")
-// const quote = new Quote()
-// const Seifer = require("../commands/seifer")
-// const seifer = new Seifer()
 
 class CommandHandler {
   constructor() {
@@ -34,72 +22,6 @@ CommandHandler.prototype.getCommand = function(msg) {
   } else {
     msg.reply(constants.errors.MISSING_COMMAND["english"](command.join(" ")))
   }
-  // switch(commandtext[0]) {
-  // case "!color":
-  // case "!colour":
-  //   commands.color.change(msg)
-  //   break
-  // case "!coult":
-  //   coult.trapCard(msg)
-  //   break
-  // case "!erika":
-  // case "!franrosave":
-  //   msg.channel.sendMessage("I am dropping the bomb :bomb:")
-  //     .then(sentmsg => setTimeout(function() {
-  //       sentmsg.edit("I am dropping the bomb :boom:")
-  //     }, 1500))
-  //     .catch(winston.error)
-  //   break
-  // case "!ayuda":
-  // case "!help":
-  //   msg.channel.sendMessage(constants.Help[language])
-  //   break
-  // case "!josde":
-  //   msg.channel.sendMessage("wew")
-  //   break
-  // case "!logoff":
-  //   if(this.admin.contains(msg.author.username)) {
-  //     msg.client.destroy()
-  //   }
-  //   break
-  // case "!logoffvoice":
-  //   if(this.admin.contains(msg.author.username)) {
-  //     if(msg.member.voiceChannel) {
-  //       msg.member.voiceChannel.leave()
-  //     }
-  //   }
-  //   break
-  // case "!magic8ball":
-  // case "!8":
-  // case "!8ball":
-  // case "!magic8":
-  //   magic8ball.answer(msg, language)
-  //   break
-  // case "!youtube":
-  //   youtube.main(msg)
-  //   break
-  // case "!podemos":
-  // case "!ciudadanos":
-  //   msg.reply("Ese partido no existe")
-  //   break
-  // case "!quote":
-  //   quote.getQuote(msg)
-  //   break
-  // case "+quote":
-  //   quote.addQuote(msg)
-  //   break
-  // case "-quote":
-  //   quote.delQuote(msg)
-  //   break
-  // case "!seifer":
-  // case "!seif":
-  // case "!diavolo":
-  //   seifer.pepe(msg)
-  //   break
-  // default:
-  //   msg.reply("Ese comando no existe")
-  //   break
-  // }
 }
 
 module.exports = CommandHandler
