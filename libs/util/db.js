1594170c7bd673fd44571fcaa05fc8439a39d7a8
DrSmugleaf <javier.frayluis@gmail.com>
2016-12-18 23:55:10 +0100

    Disabled !quote and db.js

--- libs/util/db.js
+++ libs/util/db.js
@@ -2,40 +2,40 @@
 // Copyright (c) 2016 DrSmugleaf
 //
 
-"use strict"
-const winston = require("winston")
-const PGP = require("pg-promise")({
-  error: (e) => {
-    if(e.name === "QueryResultError") { return }
-    winston.error(e)
-  }
-})
-
-const pgp = new PGP(process.env.DATABASE_URL + "?ssl=true")
-
-class DB {
-  constructor() {
-    this.qrm = {
-      one: 1,
-      many: 2,
-      none: 4,
-      any: 6
-    }
-  }
-}
-
-DB.prototype.query = function(text, values = null, mode = "any") {
-  return pgp.query(text, values, this.qrm[mode])
-}
-
-DB.prototype.cleanTable = function(table) {
-  let query = `ALTER TABLE ${table} DROP CONSTRAINT ${table}_pkey;
-CREATE TEMPORARY SEQUENCE temp_seq;
-UPDATE ${table} SET id = nextval('temp_seq');
-ALTER TABLE ${table} ADD PRIMARY KEY(id);
-DROP SEQUENCE temp_seq;
-SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}));`
-  pgp.query(query)
-}
-
-module.exports = DB
+// "use strict"
+// const winston = require("winston")
+// const PGP = require("pg-promise")({
+//   error: (e) => {
+//     if(e.name === "QueryResultError") { return }
+//     winston.error(e)
+//   }
+// })
+//
+// const pgp = new PGP(process.env.DATABASE_URL + "?ssl=true")
+//
+// class DB {
+//   constructor() {
+//     this.qrm = {
+//       one: 1,
+//       many: 2,
+//       none: 4,
+//       any: 6
+//     }
+//   }
+// }
+//
+// DB.prototype.query = function(text, values = null, mode = "any") {
+//   return pgp.query(text, values, this.qrm[mode])
+// }
+//
+// DB.prototype.cleanTable = function(table) {
+//   let query = `ALTER TABLE ${table} DROP CONSTRAINT ${table}_pkey;
+// CREATE TEMPORARY SEQUENCE temp_seq;
+// UPDATE ${table} SET id = nextval('temp_seq');
+// ALTER TABLE ${table} ADD PRIMARY KEY(id);
+// DROP SEQUENCE temp_seq;
+// SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}));`
+//   pgp.query(query)
+// }
+//
+// module.exports = DB
