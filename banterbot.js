fe8d956c90479c02248c00a3c0cfcb80c5575f69
DrSmugleaf <javier.frayluis@gmail.com>
2016-12-22 22:00:14 +0100

    Cleanup

--- banterbot.js
+++ banterbot.js
@@ -24,30 +24,30 @@ client
   .on("ready", () => {
     winston.info(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
   })
-  .on("disconnect", () => { console.warn("Disconnected!"); })
-  .on("reconnect", () => { console.warn("Reconnecting..."); })
+  .on("disconnect", () => { console.warn("Disconnected!") })
+  .on("reconnect", () => { console.warn("Reconnecting...") })
   .on("commandError", (cmd, err) => {
     if(err instanceof commando.FriendlyError) return;
-    winston.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+    winston.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err)
   })
   .on("commandBlocked", (msg, reason) => {
     winston.info(oneLine`
   		Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""}
   		blocked; ${reason}
-  	`);
+  	`)
   })
   .on("commandPrefixChange", (guild, prefix) => {
     winston.info(oneLine`
   		Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`}
   		${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
-  	`);
+  	`)
   })
   .on("commandStatusChange", (guild, command, enabled) => {
     winston.info(oneLine`
   		Command ${command.groupID}:${command.memberName}
   		${enabled ? "enabled" : "disabled"}
   		${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
-  	`);
+  	`)
   })
   .on("groupStatusChange", (guild, group, enabled) => {
     winston.info(oneLine`
@@ -55,7 +55,10 @@ client
   		${enabled ? "enabled" : "disabled"}
   		${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
   	`);
-  });
+  })
+  .on("message", (msg) => {
+    messagehandler.handle(msg)
+  })
 
 client.setProvider(
   new commando.SQLiteProvider(new PostgreSQLProvider(
