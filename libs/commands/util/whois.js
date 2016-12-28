5312c07500fa04e21e6c2bfd98963af555fa94fa
DrSmugleaf <javier.frayluis@gmail.com>
2016-12-28 20:35:13 +0100

    Added !whois command
    Gets information about a guild member

---
+++ libs/commands/util/whois.js
@@ -0,0 +1,52 @@
+//
+// Copyright (c) 2016 DrSmugleaf
+//
+
+"use strict"
+const commando = require("discord.js-commando")
+const stripIndents = require("common-tags").stripIndents
+
+module.exports = class WhoIs extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "whois",
+      aliases: ["user", "user-info", "userinfo", "whois"],
+      group: "util",
+      memberName: "whois",
+      description: "Get information about a user",
+      examples: ["whois DrSmugleaf", "whois 109067752286715904"],
+      guildOnly: true,
+      args: [
+        {
+          key: "user",
+          label: "user",
+          prompt: "What user would you like the information of?",
+          type: "string",
+          default: "yourself"
+        }
+      ]
+    })
+  }
+
+  async run(msg, args) {
+    const member = args.user == "yourself" ? msg.guild.member(msg.author) : msg.guild.member(args.user)
+
+    if(member) {
+      const user = member.user
+      return msg.reply(stripIndents`
+        Info on **${user.username}#${user.discriminator}** (ID: ${user.id})
+        **❯ Member Details**
+        ${member.nickname !== null ? ` • Nickname: ${member.nickname}` : " • No Nickname"}
+         • Roles: ${member.roles.map(roles => `\`${roles.name}\``).join(", ")}
+         • Joined at: ${member.joinedAt}
+
+        **❯ User Details**
+         • Created at: ${user.createdAt}${user.bot ? "\n • Is a bot account" : ""}
+         • Status: ${user.presence.status}
+         • Game: ${user.presence.game ? user.presence.game.name : "None"}
+      `)
+    } else {
+      return msg.reply(`User \`${args.user}\` couldn't be found`)
+    }
+  }
+}
