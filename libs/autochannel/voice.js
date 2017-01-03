689c617928edffeed85f79f411561a6aeba91681
DrSmugleaf <javier.frayluis@gmail.com>
2017-01-03 21:22:08 +0100

    Added AutoVoiceChannel check to presence updates
    Checks if a channel should be created or deleted every time someone
    comes online or changes the game they are playing

--- libs/autochannel/voice.js
+++ libs/autochannel/voice.js
@@ -32,6 +32,37 @@ class VoiceAutoChannel {
         }
       })
     })
+
+    this.discord.on("presenceUpdate", (oldMember, newMember) => {
+      const oldGame = oldMember.presence.game
+      const newGame = newMember.presence.game
+
+      if(oldGame) {
+        winston.info(oldGame && oldMember.guild.presences.filter(function(presence) {
+          return presence.game && presence.game.name && oldGame.name && presence.game.name == oldGame.name
+        }))
+      }
+
+      if(oldGame && oldMember.guild.presences.filter(function(presence) {
+        return presence.game && presence.game.name && oldGame.name && presence.game.name == oldGame.name
+      }).size < 2) {
+        const oldChannel = oldMember.guild.channels.find("name", `BB-${oldGame.name}`)
+        winston.info(oldGame.name)
+        winston.info((oldChannel && oldChannel.type == "voice") ? true : false)
+        if(oldChannel && oldChannel.type == "voice" && oldChannel.members.size < 2) {
+          oldChannel.delete()
+        }
+      }
+
+      if(newGame && newMember.guild.presences.filter(function(presence) {
+        return presence.game && presence.game.name && newGame.name && presence.game.name == newGame.name
+      }).size >= 2) {
+        if(!newMember.guild.channels.exists("name", `BB-${oldGame.name}`)) {
+          newMember.guild.createChannel(`BB-${newGame.name}`, "voice")
+            .catch(winston.error)
+        }
+      }
+    })
   }
 }
 
