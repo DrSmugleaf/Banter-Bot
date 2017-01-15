77237d34adff1837ed4fdec208e6184c803cb6aa
DrSmugleaf <javier.frayluis@gmail.com>
2017-01-15 02:37:07 +0100

    Added !playing command to change the bot's playing status

---
+++ libs/commands/util/playing.js
@@ -0,0 +1,40 @@
+//
+// Copyright (c) 2017 DrSmugleaf
+//
+
+"use strict"
+const commando = require("discord.js-commando")
+const winston = require("winston")
+
+module.exports = class Playing extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "playing",
+      aliases: ["playing"],
+      group: "util",
+      memberName: "playing",
+      description: "Change the bot's playing status",
+      examples: ["playing Overwatch"],
+      args: [
+        {
+          key: "name",
+          prompt: "The game to set as playing",
+          type: "string",
+          default: ""
+        }
+      ]
+    })
+  }
+
+  hasPermission(msg) {
+    return msg.author.id === this.client.options.owner
+  }
+
+  async run(msg, args) {
+    const name = args.name ? args.name : null
+
+    msg.client.user.setGame(name).then(() => {
+      return msg.reply(`Changed the bot's game to ${name ? name : "none"}`)
+    }).catch(winston.error)
+  }
+}
