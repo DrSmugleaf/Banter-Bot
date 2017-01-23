6ee1de0aa019a3001cfdc66b2df256a6c2e39371
DrSmugleaf <javier.frayluis@gmail.com>
2017-01-23 19:30:26 +0100

    The hell is this

--- libs/commands/youtube/base/song.js
+++ libs/commands/youtube/base/song.js
@@ -6,13 +6,13 @@
 
 module.exports = class Song {
   constructor(msg, args, video) {
-    this.channel = msg.channel,
-    this.guild = msg.guild,
-    this.member = msg.member,
+    this.channel = msg.channel
+    this.guild = msg.guild
+    this.member = msg.member
     this.message = msg
 
-    this.url = args.url,
-    this.repeat = args.repeat,
+    this.url = args.url
+    this.repeat = args.repeat
     this.repeated = false
 
     this.video = video
