deefc25915f50c92e95d9e9920b71be6559be53d
DrSmugleaf <javier.frayluis@gmail.com>
2017-01-30 22:01:03 +0100

    Removed version announcements in development environment

--- libs/announcer/version.js
+++ libs/announcer/version.js
@@ -13,17 +13,18 @@ module.exports = class Version {
   }
 
   checkVersion() {
+    if(process.env.NODE_ENV === "dev") return
     const version = this.client.settings.get("version")
 
     if(version < Object.keys(constants.versions).length + 1) {
       this.client.guilds.forEach((guild) => {
-        if(process.env.NODE_ENV === "dev" && guild.id !== "260158980343463937") return
+
         const infoChannel = guild.channels.get(guild.settings.get("info-channel", guild.defaultChannel.id))
         infoChannel.sendMessage(stripIndents`@everyone
           ${constants.versions[version + 1][guild.language]}
         `)
       })
-      if(!process.env.NODE_ENV === "dev") this.client.settings.set("version", version + 1)
+      this.client.settings.set("version", version + 1)
     }
   }
 }
