b44700de0460ddac4983ceb34b013f08972ac68c
DrSmugleaf <javier.frayluis@gmail.com>
2017-02-15 00:19:13 +0100

    Added !color remove color and missing name color tests to unit test

--- test/commands/user/color.js
+++ test/commands/user/color.js
@@ -50,6 +50,7 @@ describe("Color", function() {
 
   describe("edit", function() {
     const color = "#" + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6)
+
     it("should edit the bot's role color to another color and return a discord reply", function() {
       return global.channel.sendMessage(`!!color ${color}`).then((initialMsg) => {
         const command = global.client.dispatcher.parseMessage(initialMsg)
@@ -57,7 +58,7 @@ describe("Color", function() {
 
         return global.channel.awaitMessages((m) => {
           return m.content !== `!!color ${color}` && m.author.id === global.client.user.id
-        }, { maxMatches: 1 } ).then((msg) => {
+        }, { maxMatches: 1 }).then((msg) => {
           const reply = msg.first().content.replace(`<@${global.client.user.id}>, `, "")
           const roleColor = global.guild.member(global.guild.client.user).roles.find(
             "name", `color-${global.client.user.id}`
@@ -66,6 +67,43 @@ describe("Color", function() {
           // msg.argString has a space at the beggining
           expect(reply).to.equal(global.constants.responses.COLOR.EDITED["english"](` ${color}`))
           expect(color).to.equal(roleColor)
+        })
+      })
+    })
+  })
+
+  describe("remove", function() {
+
+    it("should remove the bot's role color and return a discord reply", function() {
+      return global.channel.sendMessage("!!color").then((initialMsg) => {
+        const command = global.client.dispatcher.parseMessage(initialMsg)
+        command.run()
+
+        return global.channel.awaitMessages((m) => {
+          return m.content !== "!!color" && m.author.id === global.client.user.id
+        }, { maxMatches: 1 }).then((msg) => {
+          const reply = msg.first().content.replace(`<@${global.client.user.id}>, `, "")
+          const roleColor = global.guild.member(global.guild.client.user).roles.find(
+            "name", `color-${global.client.user.id}`
+          )
+
+          expect(reply).to.equal(global.constants.responses.COLOR.REMOVED["english"])
+          expect(roleColor).to.not.exist
+        })
+      })
+    })
+
+    it("should return a discord reply saying the user has no name color", function() {
+      return global.channel.sendMessage("!!color").then((initialMsg) => {
+        const command = global.client.dispatcher.parseMessage(initialMsg)
+        command.run()
+
+        return global.channel.awaitMessages((m) => {
+          return m.content !== "!!color" && m.author.id === global.client.user.id
+        }, { maxMatches: 1 }).then((msg) => {
+          const reply = msg.first().content.replace(`<@${global.client.user.id}>, `, "")
+
+          expect(reply).to.equal(global.constants.responses.COLOR.NO_COLOR_ROLE["english"])
         })
       })
     })
