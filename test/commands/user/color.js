eb52fd5c8028d2d7556801d443f162ce21b53b18
DrSmugleaf <javier.frayluis@gmail.com>
2017-02-15 00:01:42 +0100

    Added !color role edit to the unit test

--- test/commands/user/color.js
+++ test/commands/user/color.js
@@ -31,4 +31,27 @@ describe("Color", function() {
       expect(color).to.equal(roleColor)
     })
   })
+
+  describe("edit", function() {
+    const color = "#" + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6)
+    it("should edit the bot's role color to another color and return a discord reply", function() {
+      return global.channel.sendMessage(`!!color ${color}`).then((initialMsg) => {
+        const command = global.client.dispatcher.parseMessage(initialMsg)
+        command.run()
+
+        return global.channel.awaitMessages((m) => {
+          return m.content !== `!!color ${color}` && m.author.id === global.client.user.id
+        }, { maxMatches: 1 } ).then((msg) => {
+          const reply = msg.first().content.replace(`<@${global.client.user.id}>, `, "")
+          const roleColor = global.guild.member(global.guild.client.user).roles.find(
+            "name", `color-${global.client.user.id}`
+          ).hexColor.toUpperCase()
+
+          // msg.argString has a space at the beggining
+          expect(reply).to.equal(global.constants.responses.COLOR.ADDED["english"](` ${color}`))
+          expect(color).to.equal(roleColor)
+        })
+      })
+    })
+  })
 })
