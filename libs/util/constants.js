80d6cf302d115c6067bb735ece8a252a5c1625c5
DrSmugleaf <javier.frayluis@gmail.com>
2017-02-16 00:34:16 +0100

    Removed !markov command
    Removed markov from dependencies
    Removed markov responses from constants

--- libs/util/constants.js
+++ libs/util/constants.js
@@ -1177,17 +1177,6 @@ exports.responses = {
     spanish: ["En mi opinión, sí.", "Es cierto.", "Es decididamente así.", "Probablemente.", "Buen pronóstico.", "Todo apunta a que sí.", "Sin duda.", "Sí.", "Sí - definitivamente.", "Debes confiar en ello.", "Respuesta vaga, vuelve a intentarlo.", "Pregunta en otro momento.", "Será mejor que no te lo diga ahora.", "No puedo predecirlo ahora.", "Concéntrate y vuelve a preguntar.", "No cuentes con ello.", "Mi respuesta es no.", "Mis fuentes me dicen que no.", "Las perspectivas no son buenas.", "Muy dudoso."]
   },
 
-  MARKOV: {
-    ANSWER: {
-      english: (subject, answer) => `**${subject} (Markov):** ${answer}.`,
-      spanish: (subject, answer) => `**${subject} (Markov):** ${answer}.`
-    },
-    MISSING: {
-      english: (user) => `There are no messages in this channel from ${user}.`,
-      spanish: (user) => `No hay mensajes en este canal de ${user}.`
-    }
-  },
-
   NAME: {
     SET: {
       english: (name) => `Changed the bot's username to ${name}.`,
