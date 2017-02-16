4121fb9e5b61c8f02613cee3d5a2985afa1396e5
DrSmugleaf <javier.frayluis@gmail.com>
2017-02-16 20:40:49 +0100

    Added BlackjackCard unit test

---
+++ test/commands/minigames/blackjack/card.js
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2017 DrSmugleaf
+//
+
+"use strict"
+const Card = require("../../../../libs/commands/minigames/blackjack/card")
+const expect = require("chai").expect
+
+describe("Card", function() {
+  it("should create a new card object", function() {
+    const card = new Card({ deck: "french", name: "Ace", value: 11, suit: {
+      name: "clubs",
+      symbol: "♣"
+    }})
+
+    expect(card.deck).to.equal("french")
+    expect(card.name).to.equal("Ace")
+    expect(card.value).to.equal(11)
+    expect(card.suit.name).to.equal("clubs")
+    expect(card.suit.symbol).to.equal("♣")
+  })
+})
