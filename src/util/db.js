bc2f0170902ba3e9795560073bbab6dcc91dc3f8
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-12 00:58:57 +0100

    Fixed db.js breaking when updating pg-promise to 5.6.4

---
+++ src/util/db.js
@@ -0,0 +1,51 @@
+//
+// Copyright (c) 2016-2017 DrSmugleaf
+//
+
+"use strict"
+const stripIndents = require("common-tags").stripIndents
+const winston = require("winston")
+const PGP = require("pg-promise")({
+  error: (e) => {
+    winston.error(e)
+  }
+})
+const pgp = PGP(process.env.NODE_ENV === "dev" ?
+  process.env.DATABASE_URL_DEV + "?ssl=true" :
+  process.env.DATABASE_URL + "?ssl=true"
+)
+
+class DB {
+  constructor() {
+    this.pgp = pgp
+    this.qrm = {
+      one: 1,
+      many: 2,
+      none: 4,
+      any: 6
+    }
+    this.cleanTableQuery = (table) => stripIndents`
+      ALTER TABLE ${table} DROP CONSTRAINT ${table}_pkey;
+      CREATE TEMPORARY SEQUENCE temp_seq;
+      UPDATE ${table} SET id = nextval('temp_seq');
+      ALTER TABLE ${table} ADD PRIMARY KEY(id);
+      DROP SEQUENCE temp_seq;
+      SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}));`
+    this.vacuumQuery = (table) => stripIndents`
+      VACUUM (ANALYZE) ${table}`
+  }
+
+  query(text, values = null, mode = "any") {
+    return this.pgp.query(text, values, this.qrm[mode])
+  }
+
+  cleanTable(table) {
+    return this.pgp.query(this.cleanTableQuery(table)) && this.vacuum(table)
+  }
+
+  vacuum(table) {
+    return this.pgp.query(this.vacuumQuery(table))
+  }
+}
+
+module.exports = DB
