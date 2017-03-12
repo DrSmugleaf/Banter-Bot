80b9f4166d28a469d1dba4e1f3b237c6c81f1b59
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-12 15:57:35 +0100

    Added check for missing environment variables on the bot side

--- src/banterbot.js
+++ src/banterbot.js
@@ -3,6 +3,7 @@
 //
 
 "use strict"
+require("checkenv").check()
 const Commando = require("discord.js-commando")
 const Discord = require("discord.js")
 require("./extensions/guild").applyToClass(Discord.Guild)
