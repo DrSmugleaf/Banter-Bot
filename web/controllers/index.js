8618e4a307202e52e5ccf8734703cf35cd522b79
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-12 16:02:45 +0100

    Added index and eve controllers

--- web/controllers/index.js
+++ web/controllers/index.js
@@ -6,4 +6,14 @@
 const express = require("express")
 const router = express.Router()
 
+router.use("/eve", require("./eve"))
+
+router.get("/", function(req, res) {
+  res.render("pages/index")
+})
+
+router.get("*", function(req, res) {
+  res.render("pages/404")
+})
+
 module.exports = router
