dfe867d2b47ec583674ea80b60626c65d50c2174
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-12 16:02:45 +0100

    Added index and eve controllers

---
+++ web/controllers/eve.js
@@ -0,0 +1,161 @@
+//
+// Copyright (c) 2017 DrSmugleaf
+//
+
+"use strict"
+const _ = require("underscore")
+const character = require("../models/eve/character")
+const contract = require("../models/eve/contract")
+const express = require("express")
+const router = express.Router()
+const request = require("request-promise")
+const session = require("express-session")
+const url = require("url")
+const uuidV4 = require("uuid/v4")
+const winston = require("winston")
+
+router.use(session({
+  secret: require("crypto").randomBytes(64).toString("hex"),
+  resave: false,
+  saveUninitialized: true,
+  maxAge: 1200000,
+}))
+
+router.get("/login", function(req, res) {
+  res.redirect(`https://login.eveonline.com/oauth/authorize/?response_type=code&redirect_uri=${process.env.EVE_CALLBACK}&client_id=${process.env.EVE_ID}`)
+})
+
+router.get("/eve", function(req, res) {
+  if(req.query.link) {
+    const link = req.query.link
+    request.get(link).then((body) => {
+      body = JSON.parse(body)
+      contract.set({
+        evepraisal_id: body.id
+        value: body.totals.sell
+        
+      })
+      }
+    })
+  }
+  res.render("pages/eve/index", {
+    character: req.session.character,
+    title: "Home - Mango Deliveries",
+    active: "Home"
+  })
+})
+
+router.get("/contracts", function(req, res) {
+  contract.getAll().then((contracts) => {
+    console.log(contracts)
+    res.render("pages/eve/contracts", {
+    character: req.session.character,
+    contracts: contracts,
+    title: "Contracts - Mango Deliveries",
+    active: "Contracts",
+    })
+  })
+})
+
+router.get("/auth", function(req, res) {
+  var eveCharacter = {}
+  request.post({
+    headers: {
+      "Authorization": `Basic ${new Buffer(`${process.env.EVE_ID}:${process.env.EVE_SECRET}`).toString("base64")}`
+    },
+    url: "https://login.eveonline.com/oauth/token",
+    form: {
+      "grant_type": "authorization_code",
+      "code": req.query.code
+    }
+  }).then((body) => {
+    body = JSON.parse(body)
+    eveCharacter.token = body.access_token
+    
+    return request.get({
+      headers: {
+        "Authorization": `Bearer ${eveCharacter.token}`
+      },
+      url: "https://login.eveonline.com/oauth/verify"
+    })
+  }).then((body) => {
+    body = JSON.parse(body)
+    eveCharacter.character_id = body.CharacterID
+    
+    return Promise.all([
+      request.get(`https://esi.tech.ccp.is/latest/characters/${body.CharacterID}/`),
+      request.get(`https://esi.tech.ccp.is/latest/characters/${body.CharacterID}/portrait/`)
+    ])
+  }).then((bodies) => {
+    _.forEach(bodies, (body, index) => {
+      bodies[index] = JSON.parse(body)
+    })
+    eveCharacter.character_name = bodies[0].name
+    eveCharacter.alliance_id = bodies[0].alliance_id
+    eveCharacter.corporation_id = bodies[0].corporation_id
+    eveCharacter.character_portrait = bodies[1].px64x64
+    
+    return Promise.all([
+      request.get(`https://esi.tech.ccp.is/latest/alliances/${eveCharacter.alliance_id}/`),
+      request.get(`https://esi.tech.ccp.is/latest/alliances/${eveCharacter.alliance_id}/icons/`),
+      request.get(`https://esi.tech.ccp.is/latest/corporations/${eveCharacter.corporation_id}/`),
+      request.get(`https://esi.tech.ccp.is/latest/corporations/${eveCharacter.corporation_id}/icons/`)
+    ])
+  }).then((bodies) => {
+    _.forEach(bodies, (body, index) => {
+      bodies[index] = JSON.parse(body)
+    })
+    eveCharacter.alliance_name = bodies[0].alliance_name
+    eveCharacter.alliance_portrait = bodies[1].px64x64
+    eveCharacter.corporation_name = bodies[2].corporation_name
+    eveCharacter.corporation_portrait = bodies[3].px64x64
+    eveCharacter.id = uuidV4()
+    
+    character.set(eveCharacter)
+    req.session.character = eveCharacter
+    res.redirect("/eve/eve")
+  }).catch((e) => {
+    winston.error(`Error while retrieving character: ${e}`)
+    res.render("pages/404")
+  })
+})
+
+router.get("/query", function(req, res) {
+  var link = url.parse(req.query.link)
+  const multiplier = req.query.multiplier || 1
+  
+  if(link && ["evepraisal.com", "skyblade.de"].includes(link.hostname)) {
+    request.get({
+      url: `${link.href}.json`
+    }).then((body) => {
+      body = JSON.parse(body)
+      
+      res.json({
+        result: (body.totals.sell * multiplier).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+        resultShort: nFormatter(body.totals.sell, 2, multiplier)
+      })
+    }).catch((e) => {
+      winston.error(e)
+    })
+  }
+})
+
+module.exports = router
+
+function nFormatter(num, digits, multiplier = 1) {
+  num = num * multiplier
+  var si = [
+    { value: 1E18, symbol: "E" },
+    { value: 1E15, symbol: "P" },
+    { value: 1E12, symbol: "T" },
+    { value: 1E9,  symbol: "B" },
+    { value: 1E6,  symbol: "M" },
+    { value: 1E3,  symbol: "k" }
+  ], rx = /\.0+$|(\.[0-9]*[1-9])0+$/, i;
+  for (i = 0; i < si.length; i++) {
+    if (num >= si[i].value) {
+      return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+    }
+  }
+  return num.toFixed(digits).replace(rx, "$1");
+}
