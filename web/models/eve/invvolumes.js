273d6d5789bd9f3e4db6ee934db955afa7998ea9
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-26 21:12:49 +0200

    Added final newlines and removed irrelevant spaces

--- web/models/eve/invvolumes.js
+++ web/models/eve/invvolumes.js
@@ -8,7 +8,7 @@ const winston = require("winston")
 
 module.exports = {
   db: null,
-  
+
   async init(db) {
     this.db = db
     await this.db.query("SELECT * FROM invvolumes LIMIT 1").catch(() => {
@@ -16,7 +16,7 @@ module.exports = {
       process.exit(1)
     })
   },
-  
+
   get(id) {
     return this.db.query("SELECT * FROM invvolumes WHERE typeID = ? LIMIT 1", [id])
   }
