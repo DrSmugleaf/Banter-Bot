cf8a3d42f80ebf8280a0582b21df2da8c5523829
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-26 22:34:08 +0200

    Added error when unable to connect to web db

--- web/db.js
+++ web/db.js
@@ -9,6 +9,8 @@ const path = require("path")
 const models = require("require-all")({
   dirname: path.join(__dirname, "/models/eve")
 })
+const stripIndents = require("common-tags").stripIndents
+const winston = require("winston")
 
 module.exports = MySQL.createConnection({
   host: process.env.MYSQL_HOST,
@@ -20,4 +22,16 @@ module.exports = MySQL.createConnection({
   _.forEach(models, (model) => {
     model.init(connection)
   })
+}).catch((e) => {
+  winston.error(stripIndents`
+    Error connecting to database ${process.env.MYSQL_DATABASE},
+    with host ${process.env.MYSQL_HOST},
+    at port ${process.env.MYSQL_PORT},
+    as user ${process.env.MYSQL_USER},
+    with password defined in the MYSQL_PASSWORD environment variable.
+    
+    Error message: ${e.name}: ${e.message}
+    Error stack trace: ${e.stack}
+  `)
+  process.exit(1)
 })
