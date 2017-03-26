5a1454a84f3996f0fdcca9a5406b368b8a5bf2b9
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-26 19:33:10 +0200

    Added function to fix ship volumes in contracts

--- web/helpers/eve.js
+++ web/helpers/eve.js
@@ -3,11 +3,14 @@
 //
 
 "use strict"
+const _ = require("underscore")
 const async = require("async")
 const invMarketGroups = require("../models/eve/invmarketgroups")
 const invTypes = require("../models/eve/invtypes")
+const invVolumes = require("../models/eve/invvolumes")
 const request = require("request-promise")
 const url = require("url")
+const winston = require("winston")
 
 module.exports = {
   nFormatter(num, digits) {
@@ -31,37 +34,72 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const link = url.parse(req.query.link)
       const multiplier = req.query.multiplier ? parseInt(req.query.multiplier, 10) : 1
+      
       if(!link) return resolve({ invalid: { "#link": "Invalid link." } })
-      if(!(link && ["evepraisal.com", "skyblade.de"].includes(link.hostname))) {
+      if(!["evepraisal.com", "skyblade.de"].includes(link.hostname)) {
         return resolve({ invalid: { "#link": "Invalid link." } })
       }
       if(!multiplier) return resolve({ invalid: { "#multiplier": "Invalid multiplier." } })
-      
+    
+      var response = { invalid: {} }
+      var appraisal
       request.get(`${link.href}.json`).then((body) => {
-        body = JSON.parse(body)
-        var response = { invalid: { "#link": "" } }
+        appraisal = JSON.parse(body)
         
-        if(body.market_name !== "Jita") {
+        return Promise.all([
+          this.filterBannedItems(appraisal),
+          this.fixShipVolumes(appraisal)
+        ])
+      }).then((promises) => {
+        const bannedItems = promises[0]
+        
+        if(appraisal.market_name !== "Jita") {
           response.invalid["#link"] += "Appraisal market must be Jita.\n"
         }
-        if(body.totals.volume * multiplier > 300000) {
+        if(appraisal.totals.volume * multiplier > 300000) {
           response.invalid["#link"] += "Total cargo volume is over 300.000m³.\n"
         }
-        async.filter(body.items, async (item, callback) => {
-          item = await invTypes.get(item.typeID)
-          const group = await invMarketGroups.getHighestParentID(item[0].marketGroupID)
-          // 475: Manufacture & Research
-          // 2: Blueprints
-          callback(null, [475, 2].includes(group[0].marketGroupID))
-        }, function(e, results) {
-          if(results.length > 0) {
-            response.invalid["#link"] += "Your appraisal contains items from the Manufacture & Research or Blueprints market groups.\n"
-          }
-          if(response.invalid) return resolve(response)
-          return resolve(body)
-        })
+        if(bannedItems.length > 0) {
+          response.invalid["#link"] += "Your appraisal contains items from the Manufacture & Research or Blueprints market groups.\n"
+        }
+        
+        if(!_.isEmpty(response.invalid)) return resolve(response)
+        return resolve(appraisal)
       }).catch((e) => {
+        winston.error(e)
         resolve({ invalid: { "#link": "Invalid appraisal." } })
+      })
+    })
+  },
+  
+  async filterBannedItems(appraisal) {
+    // 2: Blueprints
+    // 475: Manufacture & Research
+    const banned = [2, 475]
+    
+    return new Promise((resolve, reject) => {
+      async.filter(appraisal.items, async (item, callback) => {
+        item = await invTypes.get(item.typeID)
+        const group = await invMarketGroups.getHighestParentID(item[0].marketGroupID)
+        callback(null, banned.includes(group[0].marketGroupID))
+      }, function(e, results) {
+        resolve(results)
+      })
+    })
+  },
+  
+  fixShipVolumes(appraisal) {
+    appraisal.totals.volume = 0
+    
+    return new Promise((resolve, reject) => {
+      async.eachOf(appraisal.items, async (item, key, callback) => {
+        const invVolumesItem = await invVolumes.get(item.typeID)
+        if(invVolumesItem.length < 1) return callback()
+        appraisal.items[key].volume = invVolumesItem[0].volume
+        appraisal.totals.volume += invVolumesItem[0].volume
+        return callback()
+      }, function() {
+        resolve()
       })
     })
   }
