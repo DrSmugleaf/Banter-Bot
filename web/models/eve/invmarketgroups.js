7d3186c4dad2ab97e0ab8089b6a54e08be8fd021
DrSmugleaf <javier.frayluis@gmail.com>
2017-03-31 18:41:36 +0200

    Added director panel freighter panel

--- web/models/eve/invmarketgroups.js
+++ web/models/eve/invmarketgroups.js
@@ -15,6 +15,30 @@ module.exports = {
       winston.error(`MySQL table invmarketgroups in database ${process.env.MYSQL_DATABASE} doesn't exist. Please import it from CCP's latest database dump.`)
       process.exit(1)
     })
+    return this.db.query(`CREATE TABLE IF NOT EXISTS eve_banned_market_groups (
+      marketGroupID int(11) NOT NULL PRIMARY KEY,
+      marketGroupName varchar(100) DEFAULT NULL
+    )`)
+  },
+  
+  allow(id) {
+    return this.db.query("DELETE FROM eve_banned_market_groups WHERE id = ?", [id])
+  },
+  
+  ban(data) {
+    return this.db.query("INSERT INTO eve_banned_market_groups (?)", [data])
+  },
+  
+  get(id) {
+    return this.db.query("SELECT * FROM invmarketgroups WHERE typeID = ? LIMIT 1", [id])
+  },
+  
+  getBanned() {
+    return this.db.query("SELECT * FROM eve_banned_market_groups")
+  },
+  
+  getByName(name) {
+    return this.db.query("SELECT * FROM invmarketgroups WHERE typeName = ?", [name])
   },
 
   getHighestParentID(id) {
