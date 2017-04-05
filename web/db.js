db3522488771e070f96d4f7f57822a0bd169a8d6
DrSmugleaf <javier.frayluis@gmail.com>
2017-04-06 01:10:02 +0200

    Environment variable for mysql unix socket path

--- web/db.js
+++ web/db.js
@@ -18,7 +18,8 @@ const pool = MySQL.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD || "",
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  port: process.env.MYSQL_PORT,
+  socketPath: process.env.MYSQL_SOCKET_PATH
 })
 
 pool.getConnection().then((connection) => {
