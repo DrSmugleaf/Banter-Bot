714814ea8ed58f2fbf61a7ac73a066788a6e74ae
DrSmugleaf <javier.frayluis@gmail.com>
2017-04-23 15:44:21 +0200

    Fixed error when redirecting users not logged in

--- web/middlewares/eve.js
+++ web/middlewares/eve.js
@@ -7,7 +7,7 @@ const character = require("../models/eve/character")
 
 module.exports = {
   async eveAuth(req, res, next) {
-    if(req.session.character.token) {
+    if(req.session.character && req.session.character.token) {
       const eveCharacter = await character.getByToken(req.session.character.token)
       req.session.character = eveCharacter[0]
       return next()
