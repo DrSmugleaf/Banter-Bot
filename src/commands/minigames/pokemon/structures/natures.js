0fd0a52f6c2563ce6a9b06494ae9d4601b1882c5
DrSmugleaf <javier.frayluis@gmail.com>
2017-05-11 23:26:47 +0200

    Nature list

---
+++ src/commands/minigames/pokemon/structures/natures.js
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2017 DrSmugleaf
+//
+
+"use strict"
+
+module.exports = {
+  hardy: { raises: "none", lowers: "none" },
+  lonely: { raises: "attack", lowers: "defense" },
+  brave: { raises: "attack", lowers: "speed" },
+  adamant: { raises: "attack", lowers: "spAttack" },
+  naughty: { raises: "attack", lowers: "spDefense" },
+  bold: { raises: "defense", lowers: "attack" },
+  docile: { raises: "none", lowers: "none" },
+  relaxed: { raises: "defense", lowers: "spAttack" },
+  impish: { raises: "defense", lowers: "spAttack" },
+  lax: { raises: "defense", lowers: "spDefense" },
+  timid: { raises: "speed", lowers: "attack" },
+  hasty: { raises: "speed", lowers: "defense" },
+  serious: { raises: "none", lowers: "none" },
+  jolly: { raises: "speed", lowers: "spAttack" },
+  naive: { raises: "speed", lowers: "spDefense" },
+  modest: { raises: "spAttack", lowers: "attack" },
+  mild: { raises: "spAttack", lowers: "defense" },
+  quiet: { raises: "spAttack", lowers: "speed" },
+  bashful: { raises: "none", lowers: "none" },
+  rash: { raises: "spAttack", lowers: "spDefense" },
+  calm: { raises: "spDefense", lowers: "attack" },
+  gentle: { raises: "spDefense", lowers: "defense" },
+  sassy: { raises: "spDefense", lowers: "speed" },
+  careful: { raises: "spDefense", lowers: "spAttack" },
+  quirky: { raises: "none", lowers: "none" },
+}
